c886bf55f1dc6613181d49c1043819684e79954c
Sin Shimozono <una.veritas@me.com>
2014-06-17 09:05:35 +0900

    sram

---
+++ InternalSRAM/InetrnalSRAM.h
@@ -0,0 +1,17 @@
+/*
+ * InetrnalSRAM.h
+ *
+ *  Created on: 2014/06/17
+ *      Author: sin
+ */
+
+#ifndef INETRNALSRAM_H_
+#define INETRNALSRAM_H_
+
+class InternalSRAM : public Stream {
+
+};
+
+
+
+#endif /* INETRNALSRAM_H_ */
